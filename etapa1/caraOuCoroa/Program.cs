1d77fde197241043ab197fb958933fde0ab2de28
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2026-05-17 19:35:40 -0300

    Resolvendo mais alguns desafios

---
+++ etapa1/caraOuCoroa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        while((n = int.Parse(Console.ReadLine())) != 0)
+        {
+            string[] resultados = Console.ReadLine().Split(' ');
+            int joao = 0, maria = 0; // Variáveis para armazenar o número de vezes que cada um ganhou
+            for(int i = 0; i < n; i++)
+            {
+                if((int.Parse(resultados[i])) == 0)
+                    maria++;
+                else
+                    joao++;
+            }
+            Console.WriteLine($"Mary won {maria} times and John won {joao} times");
+        }
+    }
+}
