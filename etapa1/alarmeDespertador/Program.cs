a78cb899df5ae90abb15b5ffe0aa7921b373a7bb
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2026-05-18 19:38:48 -0300

    Resolvendo mais uns desafios

---
+++ etapa1/alarmeDespertador/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            int h1 = int.Parse(linha[0]);
+            int m1 = int.Parse(linha[1]);
+            int h2 = int.Parse(linha[2]);
+            int m2 = int.Parse(linha[3]);
+            if(h1 == 0 && m1 == 0 && h2 == 0 && m2 == 0)
+                break;
+
+            int atual = h1 * 60 + m1;
+            int alarme = h2 * 60 + m2;
+            if(alarme <= atual)
+                alarme += 24 * 60;
+
+            int total = alarme - atual;
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ etapa1/lavanderia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] linha = Console.ReadLine().Split(' ');
+        int la = int.Parse(linha[0]);
+        int lb = int.Parse(linha[1]);
+        linha = Console.ReadLine().Split(' ');
+        int sa = int.Parse(linha[0]);
+        int sb = int.Parse(linha[1]);
+        if(n >= la && n <= lb && n >= sa && n <= sb)
+            Console.WriteLine("possivel");
+        else
+            Console.WriteLine("impossivel");
+    }
+}
