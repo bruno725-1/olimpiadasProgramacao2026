d5cf65e6fc86b3ea38fc82f6f5ad5b62cb753190
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2026-05-21 09:54:55 -0300

    Resolução de mais um desafio

---
+++ etapa1/embaralhando/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            string linha = Console.ReadLine();
+            if(int.TryParse(linha, out int resultado))
+            {
+                if(resultado == 0)
+                    break;
+            }
+
+            Console.WriteLine(Fatorial(linha.Length));
+        }
+    }
+
+    public static double Fatorial(int x)
+    {
+        if(x == 1) return 1;
+        return x * Fatorial(x - 1);
+    }
+}
