99df81be27b80454715b7267d3bc52e34bdfdfaa
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2026-05-24 19:57:51 -0300

    Menos um desafio

---
+++ etapa2/adivinha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for(int i = 0; i < n; i++)
+        {
+            string[] linha = Console.ReadLine().Split(' ');
+            int numeroAlunos = int.Parse(linha[0]);
+            int numeroSecreto = int.Parse(linha[1]);
+            linha = Console.ReadLine().Split(' ');
+            int[] palpites = ToIntArray(linha, numeroAlunos);
+            int menorDistancia = int.MaxValue;
+            int vencedor = 0;
+            for(int j = 0; j < palpites.Length; j++)
+            {
+                int distancia = Math.Abs(palpites[j] - numeroSecreto);
+                if(distancia < menorDistancia)
+                {
+                    menorDistancia = distancia;
+                    vencedor = j + 1; // Posição começa em 1
+                }
+            }
+            Console.WriteLine(vencedor);
+        }
+    }
+
+    public static int[] ToIntArray(string[] dados, int tamanho)
+    {
+        int[] vetor = new int[tamanho];
+        for(int i = 0; i < tamanho; i++)
+            vetor[i] = int.Parse(dados[i]);
+
+        return vetor;
+    }
+}
