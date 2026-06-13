ce63f026c8c3e95728d9f3f37e4110844f2cd3d8
Bruno César Lima Araújo <1561719@sga.pucminas.br>
2026-06-13 10:53:53 -0300

    Desafios da Grande Final

---
+++ grandeFinal/floresFlorecemDaFranca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string linha;
+        while ((linha = Console.ReadLine()) != "*")
+        {
+            string[] palavras = linha.Split(' ');
+            char[] primeiras = new char[palavras.Length];
+            // Converter tudo para minúscula pra facilitar
+            for(int i = 0; i < palavras.Length; i++)
+            {
+                char prim = palavras[i][0];
+                if(char.IsUpper(prim))
+                    prim = char.ToLower(prim);
+                primeiras[i] = prim;
+            }
+            bool ehTaltograma = true;
+            char primeira = primeiras[0];
+            for(int i = 0; i < palavras.Length && ehTaltograma; i++)
+            {
+                if(primeiras[i] != primeira)
+                    ehTaltograma = false;
+            }
+            if(ehTaltograma)
+                Console.WriteLine("Y");
+            else
+                Console.WriteLine("N");
+        }
+    }
+}
---
+++ grandeFinal/mergulho/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string linha;
+        while ((linha = Console.ReadLine()) != null)
+        {
+            string[] valores = linha.Split(' ');
+            int n = int.Parse(valores[0]);
+            int r = int.Parse(valores[1]);
+            if(n == r)
+                Console.WriteLine("*");
+
+            valores = Console.ReadLine().Split(' ');
+            int[] voluntarios = PreencherVetor(n);
+            int[] vr = ToIntArray(valores); // vetor para armazenar as placas dos mergulhadores que retornaram
+            if (n - r > 0)
+            {
+                int[] vetor2 = new int[n - r];
+                for (int i = 0; i < n; i++)
+                {
+
+                }
+            }
+        }
+    }
+
+    public static int[] ToIntArray(string[] dados)
+    {
+        int[] vetor = new int[dados.Length];
+        for (int i = 0; i < dados.Length; i++)
+            vetor[i] = int.Parse(dados[i]);
+
+        return vetor;
+    }
+
+    public static int[] PreencherVetor(int comprimento)
+    {
+        int[] vetor = new int[comprimento];
+        for (int i = 0; i < comprimento; i++)
+            vetor[i] = i + 1;
+
+        return vetor;
+    }
+}
